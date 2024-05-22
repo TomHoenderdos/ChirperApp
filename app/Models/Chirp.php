e229259c37b5d2ccbe65f4852175d71c2cb98bfe
tomhoenderdos <hoenderdos.t@hsleiden.nl>
2024-05-22 13:19:16 +0200

    First!

---
+++ app/Models/Chirp.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * Events fired by this model.
+     */
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
